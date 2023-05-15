279e8d9a8aaca43f88ed033feeaa0a8784bc0c99
Anzor <anzor.shadzhe@gmail.com>
2023-05-15 20:41:52 +0300

    new file:   src/components/cardTopFour.jsx 	new file:   src/components/loading.jsx 	new file:   src/less/cardTopFour.less 	modified:   src/less/header.less new file:   src/less/loader.less 	new file:   src/less/mainMenu.less 	modified:   src/page/mainMenu.jsx 	new file:   src/style/cardTopFour.css 	modified:   src/style/header.css 	new file:   src/style/loading.css 	new file:   src/style/mainMenu.css

---
+++ src/components/loading.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../style/loading.css'
+
+const Loading = () => {
+    return (  
+        <>
+            <div className="loader">
+                <div className="contant_load">
+                    <div className="line_load"></div>
+                    <div className="line_load"></div>
+                    <div className="line_load"></div>
+                    <div className="line_load"></div>
+                    <div className="line_load"></div>
+                </div>
+            </div>
+        </>
+    );
+}
+ 
+export default Loading;
--- src/page/mainMenu.jsx
+++ src/page/mainMenu.jsx
@@ -1,13 +1,49 @@
-import React, { useEffect } from 'react';
+import React, { useState, useMemo } from 'react';
+import Loading from '../components/loading';
 import SwiperSlider from '../components/swiper';
+import CardTopFour from '../components/cardTopFour';    
+import '../style/mainMenu.css'
 
 const MainMenu = () => {
+    const [defaultData, modifiedData] = useState([]);
+
     const style = {
         marginTop: 40,
-    }
+    };
+
+    const getData = async () => {
+        const data = await fetch('src/json/data-base.json').then(response => response.json()).then(obj => obj);
+        modifiedData(data);
+    };
+
+    useMemo(() => {
+        getData();
+    }, []);
+
+    if (!defaultData.length) {
+        return (
+            <main style={style} className='main'>
+                <Loading/>
+            </main>
+        );
+    };
+
     return (
         <main style={style} className='main'>
-            <SwiperSlider/>
+            <section>
+                <SwiperSlider/>
+            </section>
+            
+            <section>
+                <h2>TOP 4</h2>
+                    <div className='container_card_top_four'>
+                        <CardTopFour dataB={defaultData} id={0}/>
+                        <CardTopFour dataB={defaultData} id={1}/>
+                        <CardTopFour dataB={defaultData} id={2}/>
+                        <CardTopFour dataB={defaultData} id={3}/>
+                    </div>
+            </section>
+            
         </main>
     );
 }
