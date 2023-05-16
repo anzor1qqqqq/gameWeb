33e2bbfe5d0b707e14607944fb13ba43930abe05
Anzor <anzor.shadzhe@gmail.com>
2023-05-16 22:50:48 +0300

    new file:   src/components/cardProduct.jsx 	deleted:    src/components/cardTopFour.jsx 	modified:   src/global/redux.js 	modified:   src/less/cardTopFour.less 	modified:   src/less/mainMenu.less 	modified:   src/page/mainMenu.jsx 	modified:   src/style/cardTopFour.css 	modified:   src/style/mainMenu.css

--- src/page/mainMenu.jsx
+++ src/page/mainMenu.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useMemo } from 'react';
 import Loading from '../components/loading';
 import SwiperSlider from '../components/swiper';
-import CardTopFour from '../components/cardTopFour';    
+import CardProduct from '../components/cardProduct'; 
 import '../style/mainMenu.css'
 
 const MainMenu = () => {
@@ -36,13 +36,33 @@ const MainMenu = () => {
             
             <section className='section_contant_main'>
                 <h2 className='section_contant_main_title'>Топ 4</h2>
-                    <div className='container_card_top_four'>
-                        <CardTopFour dataB={defaultData} id={0}/>
-                        <CardTopFour dataB={defaultData} id={1}/>
-                        <CardTopFour dataB={defaultData} id={2}/>
-                        <CardTopFour dataB={defaultData} id={3}/>
-                    </div>
+
+                <div className='container_card_top_four'>
+                    <CardProduct dataB={defaultData} id={0} spanText={'Топ 4'}/>
+                    <CardProduct dataB={defaultData} id={1} spanText={'Топ 4'}/>
+                    <CardProduct dataB={defaultData} id={2} spanText={'Топ 4'}/>
+                    <CardProduct dataB={defaultData} id={3} spanText={'Топ 4'}/>
+                </div>
             </section>
+
+            <section className='section_contant_main'>
+                <h2 className='section_contant_main_title'>Все товары</h2>
+
+                <div className='container_card_all'>
+                    <CardProduct dataB={defaultData} id={9}/>
+                    <CardProduct dataB={defaultData} id={7} spanText={'Новинкa'}/>
+                    <CardProduct dataB={defaultData} id={2} spanText={'Топ 4'}/>
+                    <CardProduct dataB={defaultData} id={10}/>
+                    <CardProduct dataB={defaultData} id={0} spanText={'Топ 4'}/>
+                    <CardProduct dataB={defaultData} id={6} spanText={'Хит продаж'}/>
+                    <CardProduct dataB={defaultData} id={1} spanText={'Топ 4'}/>
+                    <CardProduct dataB={defaultData} id={4} spanText={'Новинкa'}/>
+                    <CardProduct dataB={defaultData} id={3} spanText={'Топ 4'}/>
+                    <CardProduct dataB={defaultData} id={5}/>
+                </div>
+
+                <button className='btn_link_catalogy'>Перейти в каталог</button>
+            </section> 
             
         </main>
     );
