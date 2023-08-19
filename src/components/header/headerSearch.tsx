f90fc67a76c6daa662925afca32ef832db985336
Anzor <anzor.shadzhe@gmail.com>
2023-08-19 12:10:19 +0300

    modified:   src/page/static/static.tsx 	modified:   src/types/types.ts

--- src/components/header/headerSearch.tsx
+++ src/components/header/headerSearch.tsx
@@ -5,16 +5,23 @@ import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import { FC } from 'react';
-import { IPropsHeaderSearch, IStateStore } from '../../types/types';
+import { IPropsHeaderSearch, IStateStore, ILoaderData } from '../../types/types';
 
 import logoBall from '../../img/logoBall.png';
 import '../../style/header.css';
 
 const HeaderSearch: FC<IPropsHeaderSearch> = ({callback}): JSX.Element => {
+    const [dataB, setDataB] = React.useState<ILoaderData[]>([]);
+
     const [switchToInput, setSwitchToInput] = React.useState(false);
     const stateValue = useSelector((state: IStateStore) => state);
 
+
     const switchStatus = () => setSwitchToInput(prev => !prev);
+
+    const searchProduct = () => {
+
+    };
 
     if (switchToInput) {
         return (
@@ -35,7 +42,7 @@ const HeaderSearch: FC<IPropsHeaderSearch> = ({callback}): JSX.Element => {
 
     return (  
         <div className='header_search'>
-                    <div className='burger_menu' onClick={() => callback()}>
+                    <div className='burger_menu' onClick={callback}>
                         <span className='burger_menu_line'></span>
                         <span className='burger_menu_line'></span>
                         <span className='burger_menu_line'></span>
@@ -49,7 +56,7 @@ const HeaderSearch: FC<IPropsHeaderSearch> = ({callback}): JSX.Element => {
                     </div>
 
                     <div className='header_search_contant_input'>
-                        <input className='header_search_contant_input-input' type="text" placeholder='Поиск' />
+                        <input className='header_search_contant_input-input' type="text" placeholder='Поиск' onChange={searchProduct}/>
                         <div className='header_search_contant_input_img'>
                             <svg className='icon_svg'   viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path fillRule="evenodd" clipRule="evenodd" d="M21.9992 11.6472C21.9992 16.9753 17.6804 21.2943 12.3532 21.2943C7.026 21.2943 2.70714 16.9753 2.70714 11.6472C2.70714 6.319 7.026 2 12.3532 2C17.6804 2 21.9992 6.319 21.9992 11.6472ZM23.9992 11.6472C23.9992 18.0797 18.7851 23.2943 12.3532 23.2943C9.49714 23.2943 6.88123 22.2661 4.85532 20.5597L1.41428 24.0011L0 22.5869L3.4412 19.1454C1.7351 17.1193 0.707141 14.5033 0.707141 11.6472C0.707141 5.21461 5.92125 0 12.3532 0C18.7851 0 23.9992 5.21461 23.9992 11.6472Z" fill="white" fillOpacity="0.2"/>
